b29609f54ce7c5cdcfc7b4ff022481371072b8f6
Andrre Lima <andre.salatalima@gmail.com>
2023-08-15 21:59:06 +0200

    user defs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only:[:show]
+
   def index
     @should_render_navbar = true
   end
+
+  def show
+
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+    @user = User.find_by_id(params[:id])
 end
