7d96e789dd4687522cbc7be09bb8c52896bdcd56
Andrre Lima <andre.salatalima@gmail.com>
2023-08-18 16:37:19 +0200

    update without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def update_resource(resource, params)
+    # Require current password if user is trying to change password.
+    return super if params['password']&.present?
+
+    # Allows user to update registration information without password.
+    resource.update_without_password(params.except('current_password'))
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -59,4 +67,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+
 end
